cbff7e6c546e0910a75dffa2fa28fe3da3905e3b
Alex Salazar <alxanderapollo@gmail.com>
2021-07-15 22:22:34 -0400

    Added a short nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,20 @@
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route,Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamShow';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamShow';
 import StreamEdit from './streams/StreamShow';
-
+import Header from './Header';
 
 
 
 const  App = ()=> {
     return (<div>
+        
     <BrowserRouter>
         <div>
+        <Header/>
             <Route path="/" exact component={StreamList}/>
             <Route path="/streams/new" exact component={StreamCreate}/>
             <Route path="/streams/edit" exact component={StreamEdit}/>
