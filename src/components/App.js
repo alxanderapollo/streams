98bce188d9fa923f8823cf9ff0085646793af98b
Alex Salazar <alxanderapollo@gmail.com>
2021-07-15 21:52:56 -0400

    Wired up all the routes to components, ready for diaplying dummy pages

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamShow';
+import StreamEdit from './streams/StreamShow';
+
+
+
 
 const  App = ()=> {
-    return <div>Hello</div>
+    return (<div>
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact component={StreamList}/>
+            <Route path="/streams/new" exact component={StreamCreate}/>
+            <Route path="/streams/edit" exact component={StreamEdit}/>
+            <Route path="/streams/delete" exact component={StreamDelete}/>
+            <Route path="/streams/show" exact component={StreamShow}/>
+        </div>
+    </BrowserRouter>
+
+
+        
+    </div>)
 }
 
 export default App;
---
+++ src/components/streams/StreamShow.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function StreamShow() {
+    return (
+        <div>
+            StreamShow
+        </div>
+    )
+}
