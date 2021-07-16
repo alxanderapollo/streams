862118fbbd18d39dd352b82ad38135b1e731077b
Alex Salazar <alxanderapollo@gmail.com>
2021-07-15 21:52:56 -0400

    Added routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route,Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';StreamShow';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamShow';
+import StreamEdit from './streams/StreamShow';
+
+
+
 
 const  App = ()=> {
-    return <div>Hello</div>
+    return (<div>
+        <BrowserRouter>
+           
+        </BrowserRouter>
+
+        
+    </div>)
 }
 
 export default App;
---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function StreamDelete() {
+    return (
+        <div>
+            StreamDelete
+        </div>
+    )
+}
