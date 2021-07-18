f3c305f4b08d6485763ce46bdbd45cb019bb229d
Alex Salazar <alxanderapollo@gmail.com>
2021-07-18 13:56:21 -0400

    Added my own browser Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Router, Route} from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
 import StreamEdit from './streams/StreamEdit';
 import Header from './Header';
+import history from '../history'
 
 
 //Client ID: 182565464848-94dfl9fd6386iqj6lgecqt3udrkvkqbi.apps.googleusercontent.com
@@ -14,7 +15,7 @@ import Header from './Header';
 const  App = ()=> {
     return (<div>
         
-    <BrowserRouter>
+    <Router history={history}>
         <div>
         <Header/>
             <Route path="/" exact component={StreamList}/>
@@ -23,7 +24,7 @@ const  App = ()=> {
             <Route path="/streams/delete" exact component={StreamDelete}/>
             <Route path="/streams/show" exact component={StreamShow}/>
         </div>
-    </BrowserRouter>
+    </Router>
     </div>)
 }
 
