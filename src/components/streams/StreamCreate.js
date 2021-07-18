29b6faaf041778fda369d6cd80e06fe441956c1e
Alex Salazar <alxanderapollo@gmail.com>
2021-07-17 22:15:59 -0400

    Added a server

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -33,7 +33,7 @@ class StreamCreate extends React.Component{
 
     render() {
         // onsubmit has this.props.handleSubmit, and in that our helper function, this is because were using the f
-       return ( <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form" >
+       return ( <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error" >
             <Field name="title" component={this.renderInput} label="Enter Title "/>
             <Field name="description" component={this.renderInput} label="Enter Description "/>
             <button className="ui button primary">Submit</button>
