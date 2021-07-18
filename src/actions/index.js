ca8cdfc5dfa53c2b1ff48825e42c40f566d222b0
Alex Salazar <alxanderapollo@gmail.com>
2021-07-18 14:11:57 -0400

    Added Editing button that redirects user to stream edit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams'
+import history from '../history'
 import {
     SIGN_IN,SIGN_OUT,
     CREATE_STREAM,
@@ -26,6 +27,8 @@ export const createStream = formValues => async (dispatch,getState) => {
     const {userId} = getState().auth;
     const response = await streams.post('/streams', {...formValues,userId});
     dispatch({type: CREATE_STREAM, payload: response.data})
+    //navigate our user back to the home screen
+    history.push('/');
 };
 
 //get all the  streams action creator 
