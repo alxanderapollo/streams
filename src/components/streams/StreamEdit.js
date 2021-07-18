2df2b56794c07e1def040f9a60e9b275f5e573ba
Alex Salazar <alxanderapollo@gmail.com>
2021-07-18 14:48:47 -0400

    added a file that will serve as a resuable file

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,8 +1,26 @@
+//in our app.js we have a colon that comes after the route
+//what it is known as route params, or paramters passed on through the path of a website
+//the value that is passed , is used as our route param used to decide which stream is going to be edited
 import React from 'react'
-export default function StreamEdit() {
-    return (
+import {connect} from 'react-redux'
+import { fetchStream } from '../../actions';
+class StreamEdit extends React.Component {
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);
+    }
+    
+    render(){
+        if(!this.props.stream){
+            return <div>Loading</div>
+        }
+        return (
         <div>
-            StreamEdit
+            {this.props.stream.title}
         </div>
     )
+    }
 }
+const mapStateToProps = (state,ownProps) => {
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+export default connect(mapStateToProps, {fetchStream})(StreamEdit)
